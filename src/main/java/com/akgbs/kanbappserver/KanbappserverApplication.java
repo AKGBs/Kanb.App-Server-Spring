49e00e8b3217abcffd1aa6d2a852ac83319d146c
Bugli Lorenzo <bugli.lorenzo@gmail.com>
2019-08-17 18:50:57 +0200

    FIXME: Failed to start

---
+++ src/main/java/com/akgbs/kanbappserver/KanbappserverApplication.java
@@ -0,0 +1,13 @@
+package com.akgbs.kanbappserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanbappserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KanbappserverApplication.class, args);
+    }
+
+}
